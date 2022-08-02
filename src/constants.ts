15bf66cbb2d7205146699f5391272b403e4202ff
Aaron Barnard <abarnard@protonmail.com>
2022-08-02 13:31:32 +1000

    Set port and remove dotenv

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,4 @@
 // load env vars
-// eslint-disable-next-line
-const env = require('dotenv')
-env.config()
-
-export const PORT = Number(process.env.PORT)
+export const PORT = Number(process.env.PORT) || 3000
 
 export const MAX_SOCKET_BACKPRESSURE_BYTES = 1024 * 1024 // 1mb
