af372e06285979234f70d66956182fed785f9444
Aaron <abarnard@protonmail.com>
2022-10-03 09:16:43 +1100

    Add proper socket cleanup logic

--- src/websockets/close.ts
+++ src/websockets/close.ts
@@ -1,7 +1,13 @@
+import { Socket } from 'net'
 import { WebSocket } from 'uWebSockets.js'
 
 function handleClose(ws: WebSocket): void {
   ws.closed = true
+
+  // close connection to node
+  const { nodeSocket } = ws as WebSocket & { nodeSocket: Socket }
+  nodeSocket.removeAllListeners()
+  nodeSocket.end()
 }
 
 export default handleClose
