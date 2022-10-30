7f05b36dce24089b7d1fc093b4cd5c01c9ad8ded
Aaron <abarnard@protonmail.com>
2022-10-30 15:17:34 +1100

    Comma separated list

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@
 export const HOST = process.env.HOST || 'localhost'
 export const PORT = Number(process.env.PORT) || 3000
 export const MAX_SOCKET_BACKPRESSURE_BYTES = 1024 * 1024 // 1mb
-export const RESTRICT_ORIGINS = process.env.RESTRICT_ORIGINS
+export const RESTRICT_ORIGINS =
+  process.env.RESTRICT_ORIGINS && process.env.RESTRICT_ORIGINS.split(',')
